2fa0a19b560f5399155335e0e46c247b9a5a73c0
isma050605 <ismaelbuaisa@gmail.com>
2024-04-08 13:25:04 +0200

    Tercer commit clase2 Rama Master

--- src/clases/clase2.java
+++ src/clases/clase2.java
@@ -8,29 +8,34 @@ public class clase2 {
 
         // Ejemplo de uso de los métodos
         objeto.saludar();
-        System.out.println("El área de un cuadrado de lado 5 es: " + objeto.calcularAreaCuadrado(5));
-        System.out.println("El perímetro de un cuadrado de lado 5 es: " + objeto.calcularPerimetroCuadrado(5));
         System.out.println("El área de un círculo de radio 3 es: " + objeto.calcularAreaCirculo(3));
-        System.out.println("La circunferencia de un círculo de radio 3 es: " + objeto.calcularCircunferenciaCirculo(3));
+        System.out.println("El perímetro de un círculo de radio 3 es: " + objeto.calcularPerimetroCirculo(3));
+        System.out.println("El área de un triángulo con base 4 y altura 3 es: " + objeto.calcularAreaTriangulo(4, 3));
+        System.out.println("El perímetro de un triángulo con lados 3, 4 y 5 es: " + objeto.calcularPerimetroTriangulo(3, 4, 5));
+        System.out.println("El volumen de una esfera con radio 4 es: " + objeto.calcularVolumenEsfera(4));
     }
 
     public void saludar() {
-        System.out.println("¡Hola desde la clase2!");
-    }
-
-    public double calcularAreaCuadrado(double lado) {
-        return lado * lado;
-    }
-
-    public double calcularPerimetroCuadrado(double lado) {
-        return 4 * lado;
+        System.out.println("¡Hola desde la Clase2!");
     }
 
     public double calcularAreaCirculo(double radio) {
-        return Math.PI * radio * radio;
+        return Math.PI * Math.pow(radio, 2);
     }
 
-    public double calcularCircunferenciaCirculo(double radio) {
+    public double calcularPerimetroCirculo(double radio) {
         return 2 * Math.PI * radio;
+    }
+
+    public double calcularAreaTriangulo(double base, double altura) {
+        return (base * altura) / 2;
+    }
+
+    public double calcularPerimetroTriangulo(double lado1, double lado2, double lado3) {
+        return lado1 + lado2 + lado3;
+    }
+    
+    public double calcularVolumenEsfera(double radio) {
+        return (4.0/3.0) * Math.PI * Math.pow(radio, 3);
     }
 }
