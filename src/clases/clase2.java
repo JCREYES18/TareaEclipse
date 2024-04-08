36611ee98a85d5e9534467ac4dd0fd152a1d4205
isma050605 <ismaelbuaisa@gmail.com>
2024-04-08 13:20:06 +0200

    Segundo commit clase2 Rama Master

--- src/clases/clase2.java
+++ src/clases/clase2.java
@@ -3,21 +3,34 @@ package clases;
 public class clase2 {
 
     public static void main(String[] args) {
-        // Ejemplo de uso de las funciones
-        saludar();
-        System.out.println("El área de un cuadrado de lado 5 es: " + calcularAreaCuadrado(5));
-        System.out.println("El área de un círculo de radio 3 es: " + calcularAreaCirculo(3));
+        // Crear una instancia de Clase2
+        clase2 objeto = new clase2();
+
+        // Ejemplo de uso de los métodos
+        objeto.saludar();
+        System.out.println("El área de un cuadrado de lado 5 es: " + objeto.calcularAreaCuadrado(5));
+        System.out.println("El perímetro de un cuadrado de lado 5 es: " + objeto.calcularPerimetroCuadrado(5));
+        System.out.println("El área de un círculo de radio 3 es: " + objeto.calcularAreaCirculo(3));
+        System.out.println("La circunferencia de un círculo de radio 3 es: " + objeto.calcularCircunferenciaCirculo(3));
     }
 
-    public static void saludar() {
+    public void saludar() {
         System.out.println("¡Hola desde la clase2!");
     }
 
-    public static double calcularAreaCuadrado(double lado) {
+    public double calcularAreaCuadrado(double lado) {
         return lado * lado;
     }
 
-    public static double calcularAreaCirculo(double radio) {
+    public double calcularPerimetroCuadrado(double lado) {
+        return 4 * lado;
+    }
+
+    public double calcularAreaCirculo(double radio) {
         return Math.PI * radio * radio;
+    }
+
+    public double calcularCircunferenciaCirculo(double radio) {
+        return 2 * Math.PI * radio;
     }
 }
