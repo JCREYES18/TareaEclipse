791e5a1239970f450ce67aeb5b04f7292e8e0793
isma050605 <ismaelbuaisa@gmail.com>
2024-04-08 13:04:12 +0200

    Quinto commit clase1 Rama Master

--- src/clases/clase1.java
+++ src/clases/clase1.java
@@ -9,17 +9,22 @@ public class clase1 {
         System.out.println("Bienvenido al programa");
 
         while (true) {
-            System.out.println("Por favor, seleccione una opción:");
-            System.out.println("1. Sumar");
-            System.out.println("2. Restar");
-            System.out.println("3. Dividir");
-            System.out.println("4. Salir");
+            mostrarMenu();
 
-            int opcion = scanner.nextInt();
+            int opcion;
+            try {
+                opcion = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Opción no válida, por favor seleccione nuevamente.");
+                scanner.next();
+                continue;
+            }
+
             if (opcion == 4) {
                 System.out.println("Saliendo del programa...");
                 break;
             }
+
             if (opcion < 1 || opcion > 3) {
                 System.out.println("Opción no válida, por favor seleccione nuevamente.");
                 continue;
@@ -29,8 +34,16 @@ public class clase1 {
             double num1 = scanner.nextDouble();
             double num2 = scanner.nextDouble();
 
-            double resultado = (opcion == 1) ? num1 + num2 : (opcion == 2) ? num1 - num2 : num2 != 0 ? num1 / num2 : Double.POSITIVE_INFINITY;
+            double resultado = (opcion == 1) ? num1 + num2 : (opcion == 2) ? num1 - num2 : (num2 != 0) ? num1 / num2 : Double.POSITIVE_INFINITY;
             System.out.println("El resultado es: " + resultado);
         }
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("Por favor, seleccione una opción:");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Dividir");
+        System.out.println("4. Salir");
     }
 }
