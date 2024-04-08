5c5cf7d7b05db41dadebea48bb4f8cf13b9f3d80
isma050605 <ismaelbuaisa@gmail.com>
2024-04-08 13:33:57 +0200

    Cuarto commit clase2 Rama Master

--- src/clases/clase2.java
+++ src/clases/clase2.java
@@ -3,20 +3,19 @@ package clases;
 public class clase2 {
 
     public static void main(String[] args) {
-        // Crear una instancia de Clase2
         clase2 objeto = new clase2();
 
-        // Ejemplo de uso de los métodos
         objeto.saludar();
         System.out.println("El área de un círculo de radio 3 es: " + objeto.calcularAreaCirculo(3));
         System.out.println("El perímetro de un círculo de radio 3 es: " + objeto.calcularPerimetroCirculo(3));
-        System.out.println("El área de un triángulo con base 4 y altura 3 es: " + objeto.calcularAreaTriangulo(4, 3));
-        System.out.println("El perímetro de un triángulo con lados 3, 4 y 5 es: " + objeto.calcularPerimetroTriangulo(3, 4, 5));
+        System.out.println("El área de un trapecio con bases 4 y 6 y altura 3 es: " + objeto.calcularAreaTrapecio(4, 6, 3));
         System.out.println("El volumen de una esfera con radio 4 es: " + objeto.calcularVolumenEsfera(4));
+        System.out.println("El perímetro de un cuadrado con lado 5 es: " + objeto.calcularPerimetroCuadrado(5));
+        System.out.println("El perímetro de un rectángulo con base 5 y altura 6 es: " + objeto.calcularPerimetroRectangulo(5, 6));
     }
 
     public void saludar() {
-        System.out.println("¡Hola desde la Clase2!");
+        System.out.println("¡Hola desde la clase2!");
     }
 
     public double calcularAreaCirculo(double radio) {
@@ -27,15 +26,19 @@ public class clase2 {
         return 2 * Math.PI * radio;
     }
 
-    public double calcularAreaTriangulo(double base, double altura) {
-        return (base * altura) / 2;
+    public double calcularAreaTrapecio(double baseMayor, double baseMenor, double altura) {
+        return ((baseMayor + baseMenor) * altura) / 2;
     }
 
-    public double calcularPerimetroTriangulo(double lado1, double lado2, double lado3) {
-        return lado1 + lado2 + lado3;
-    }
-    
     public double calcularVolumenEsfera(double radio) {
         return (4.0/3.0) * Math.PI * Math.pow(radio, 3);
+    }
+    
+    public double calcularPerimetroCuadrado(double lado) {
+        return 4 * lado;
+    }
+    
+    public double calcularPerimetroRectangulo(double base, double altura) {
+        return 2 * (base + altura);
     }
 }
