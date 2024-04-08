0c3d56d7ae27434b85ae733a533ebdbb7eefe52c
isma050605 <ismaelbuaisa@gmail.com>
2024-04-08 13:53:06 +0200

    Segundo commit clase3 Rama Master

--- src/clases/clase3.java
+++ src/clases/clase3.java
@@ -1,7 +1,6 @@
 package clases;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class clase3 {
 
@@ -9,18 +8,25 @@ public class clase3 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese una serie de números separados por espacios:");
 
-        ArrayList<Integer> numeros = leerNumeros(scanner);
+        int[] numeros = leerNumeros(scanner);
 
-        System.out.println("Los números ingresados son: " + numeros);
+        System.out.println("Los números ingresados son: ");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+
+        double promedio = calcularPromedio(numeros);
+        System.out.println("\nEl promedio de los números ingresados es: " + promedio);
     }
 
-    public static ArrayList<Integer> leerNumeros(Scanner scanner) {
-        ArrayList<Integer> numeros = new ArrayList<>();
+    public static int[] leerNumeros(Scanner scanner) {
+        System.out.print("Números: ");
         String[] entrada = scanner.nextLine().split(" ");
+        int[] numeros = new int[entrada.length];
         
-        for (String num : entrada) {
+        for (int i = 0; i < entrada.length; i++) {
             try {
-                numeros.add(Integer.parseInt(num));
+                numeros[i] = Integer.parseInt(entrada[i]);
             } catch (NumberFormatException e) {
                 System.out.println("Error: Ingrese solo números separados por espacios.");
                 break;
@@ -28,5 +34,13 @@ public class clase3 {
         }
         
         return numeros;
+    }
+
+    public static double calcularPromedio(int[] numeros) {
+        double sumatoria = 0;
+        for (int num : numeros) {
+            sumatoria += num;
+        }
+        return sumatoria / numeros.length;
     }
 }
