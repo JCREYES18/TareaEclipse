a5395311b0b72ed464af42a4c2df83a618c72c47
JCREYES18 <juancarlosreyesgarcia2004@gmail.com>
2024-04-14 14:25:26 +0200

    Segundo Commit clase3 NuevaRama

--- src/clases/clase3.java
+++ src/clases/clase3.java
@@ -7,39 +7,42 @@ public class clase3 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Por favor, ingrese una serie de palabras separadas por espacios:");
+        System.out.println("Por favor, ingrese una serie de números separados por espacios:");
 
-        String[] palabras = scanner.nextLine().split(" ");
+        String[] numerosStr = scanner.nextLine().split(" ");
+        int[] numeros = new int[numerosStr.length];
 
-        System.out.println("Las palabras ingresadas son: " + Arrays.toString(palabras));
+        for (int i = 0; i < numerosStr.length; i++) {
+            numeros[i] = Integer.parseInt(numerosStr[i]);
+        }
 
-        int cantidadPalabras = palabras.length;
-        System.out.println("La cantidad de palabras ingresadas es: " + cantidadPalabras);
+        System.out.println("Los números ingresados son: " + Arrays.toString(numeros));
 
-        String palabraMasLarga = encontrarPalabraMasLarga(palabras);
-        System.out.println("La palabra más larga ingresada es: " + palabraMasLarga);
+        int cantidadNumeros = numeros.length;
+        System.out.println("La cantidad de números ingresados es: " + cantidadNumeros);
 
-        String palabraMasCorta = encontrarPalabraMasCorta(palabras);
-        System.out.println("La palabra más corta ingresada es: " + palabraMasCorta);
+        int suma = calcularSuma(numeros);
+        System.out.println("La suma de los números ingresados es: " + suma);
+
+        int maximo = encontrarMaximo(numeros);
+        System.out.println("El número máximo ingresado es: " + maximo);
     }
 
-    public static String encontrarPalabraMasLarga(String[] palabras) {
-        String palabraMasLarga = "";
-        for (String palabra : palabras) {
-            if (palabra.length() > palabraMasLarga.length()) {
-                palabraMasLarga = palabra;
-            }
+    public static int calcularSuma(int[] numeros) {
+        int suma = 0;
+        for (int num : numeros) {
+            suma += num;
         }
-        return palabraMasLarga;
+        return suma;
     }
 
-    public static String encontrarPalabraMasCorta(String[] palabras) {
-        String palabraMasCorta = palabras[0];
-        for (String palabra : palabras) {
-            if (palabra.length() < palabraMasCorta.length()) {
-                palabraMasCorta = palabra;
+    public static int encontrarMaximo(int[] numeros) {
+        int maximo = Integer.MIN_VALUE;
+        for (int num : numeros) {
+            if (num > maximo) {
+                maximo = num;
             }
         }
-        return palabraMasCorta;
+        return maximo;
     }
 }
